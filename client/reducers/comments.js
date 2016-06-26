5b489177ff283c96d8aa1e7501e8f7ead04325ce
Oregu <oleg.burykin@gmail.com>
2016-06-25 21:00:14 -0700

    Now with comments

--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -2,7 +2,33 @@
 // 1. the action (info about what happened)
 // 2. copy of current state
 
-export default function comments(state = [], action) {
-  console.log(state, action);
+function postComments(state = [], action) {
+  switch (action.type) {
+    case 'ADD_COMMENT':
+      return [...state, {
+        user: action.author,
+        text: action.comment
+      }];
+    case 'REMOVE_COMMENT':
+      return [
+        ...state.slice(0, action.index),
+        ...state.slice(action.index+1)
+      ];
+    default:
+      return state;
+  }
+}
+
+function comments(state = [], action) {
+  if (typeof action.postId !== 'undefined') {
+    return {
+      // current state
+      ...state,
+      // overwrite this post with a new one
+      [action.postId]: postComments(state[action.postId], action)
+    };
+  }
   return state;
 }
+
+export default comments;
